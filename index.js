aadc0d6e4ef124bbd0104f3eafc1a617b2c4f23d
Kennedy <kiplangatkennedy019@gmail.com>
2023-03-17 18:27:11 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-
+let num1=31;
+let num2=2;
 // Write your code here
+        //--const---
+const multiply=num1*num2;
+console.log(multiply);
+
+      //-- Alternative way to do it  is given below--
+            //   const multiply= function(num1, num2){
+            //     console.log(num1*num2);
+            // }
+            // multiply(2,31)
+
+            //--random--
+let random = Math.floor(Math.random()*5);
+console.log(random);
+
+            //----mod---
+
+let num3=29;
+let num4=5;
+let mod=num3%num4; //mod
+console.log(mod);
+ let max=Math.max(12,14,7,19,20);
+ console.log(max)
